93f7ed41ee2d519348ba1a0e5cd1fe3ddcb58726
James Sheales <james@godrics-hollow.co.uk>
2020-09-18 12:22:04 +0100

    functional menu

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class Data:
     def generate_dataset(self, size: int) -> List:
         data = []
         for x in range(size):
+            x = x # Literally to stop git and vscode complaing that x is unused
             data.append(random.randint(0, 100))
         return data
 
@@ -32,14 +33,34 @@ class Data:
 
 class Menu:
     def __init__(self):
-        self.options = ["bubble_sort", "insertion_sort"]
+        self.options = ["bubble_sort", "insertion_sort", "merge_sort", "linear_search"]
         self.show_menu()
 
+    def get_nice_name(self, option: str) -> str:
+        words = option.split("_")
+        name = f"{words[0]} {words[1]}".title()
+        return name
+
     def show_menu(self):
+        self.clear_screen()
         print(f"{15*'-'}Menu{15*'-'}")
-        for option in self.options:
-            print(option)
+        for option in range(len(self.options)):
+            print(f"{option+1}. {self.get_nice_name(self.options[option])}")
+        print(f"{option+2}. Quit")
         print(34*"-")
+        choice = int(input("> "))
+
+        if choice == len(self.options)+1:
+            self.clear_screen()
+            quit()
+        
+        elif self.options[choice-1].split("_")[1] == "sort":
+            print("sort")
+
+        else:
+            print("search")
+
+        
     def clear_screen(self):
         if name == "nt":
             system("cls")
